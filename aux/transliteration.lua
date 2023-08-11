9f23aa4be4ddc034b49940ecd42136cd6ce7e70f
jodros <15092760+jodros@users.noreply.github.com>
2023-08-11 15:22:58 -0300

    ...

--- aux/transliteration.lua
+++ aux/transliteration.lua
@@ -1,4 +1,4 @@
-local letters = {
+local latin = {
 	['a'] = 'а',
 	['A'] = 'А',
 	['ã'] = 'ань',
@@ -13,8 +13,8 @@ local letters = {
 
 	['i'] = 'и',
 	['I'] = 'И',
-	['í'] = 'и',
-	['Í'] = 'И',
+	['í'] = 'иь',
+	['Í'] = 'Иь',
 
 	['y'] = 'ы',
 	['Y'] = 'Ы',
@@ -27,6 +27,9 @@ local letters = {
 
 	['u'] = 'у',
 	['U'] = 'У',
+	['ú'] = 'уь',
+	['Ú'] = 'УЬ',
+
 	['b'] = 'б',
 	['B'] = 'Б',
 	['d'] = 'д',
@@ -68,33 +71,151 @@ local letters = {
 	['ç'] = 'ц',
 	['Ç'] = 'Ц',
 
-	-- 
+	['q'] = 'к',
+	['Q'] = 'К',
+}
 
-	['rr'] = 'х',
-	['RR'] = 'Х',
-	['qu'] = 'ку',
-	['Qu'] = 'КУ',
+local cyrrilic = {
+	['а'] = 'a',
+	['А'] = 'A',
+
+	['е'] = 'e',
+	['Е'] = 'E',
+	['э'] = 'é',
+	['Э'] = 'É',
+
+
+	['и'] = 'i',
+	['И'] = 'I',
+	['й'] = 'i',
+	['Й'] = 'I',
+
+	['ы'] = 'y',
+	['Ы'] = 'Y',
+
+	['о'] = 'o',
+
+	['у'] = 'u',
+	['У'] = 'U',
+	['б'] = 'b',
+	['Б'] = 'B',
+	['д'] = 'd',
+	['Д'] = 'D',
+	['ф'] = 'f',
+	['Ф'] = 'F',
+
+	['г'] = 'h', -- ou G???
+	['Г'] = 'H',
+
+	['ж'] = 'j',
+	['Ж'] = 'J',
+	['к'] = 'k',
+	['К'] = 'K',
+	['л'] = 'l',
+	['Л'] = 'L',
+	['м'] = 'm',
+	['М'] = 'M',
+	['н'] = 'n',
+	['Н'] = 'N',
+	['п'] = 'p',
+	['П'] = 'P',
+	['т'] = 't',
+	['Т'] = 'T',
+	['в'] = 'v',
+	['В'] = 'V',
+	['з'] = 'z',
+	['З'] = 'Z',
+	['р'] = 'r',
+	['Р'] = 'R',
+	['с'] = 's',
+	['С'] = 'S',
+	['к'] = 'c',
+	['К'] = 'C',
+	['г'] = 'g',
+	['Г'] = 'G',
+	['в'] = 'w',
+	['В'] = 'W',
+	['ц'] = 'ç',
+	['Ц'] = 'Ç',
+
+	special = {
+		['аь'] = 'á',
+		['Аь'] = 'Á',
+		['аъ'] = 'à',
+		['Аъ'] = 'À',
+		['ань'] = 'ã',
+		['АНь'] = 'Ã',
+
+		['еъ'] = 'ê',
+		['Еъ'] = 'Ê',
+
+		['иь'] = 'í',
+		['Иь'] = 'Í',
+
+		['уь'] = 'ú',
+		['Уь'] = 'Ú',
+
+		['оь'] = 'ó',
+		['Оь'] = 'Ó',
+		['оъ'] = 'ô',
+		['Оъ'] = 'Ô',
+		['онъ'] = 'õ',
+
+
+		['ку'] = 'qu',
+		['КУ'] = 'Qu',
+
+		['кс'] = 'x',
+		['КС'] = 'X',
+
+		['със'] = 'sc',
+		['СЪС'] = 'sc',
+
+	}
 	-- [' '] = ' ',
 	-- ['.'] = '.',
 	-- [','] = ',',
 	-- ['\n'] = '\n'
 }
 
+--[[
+	Some conventions:
+	- The nasalisation mark...
+	- Circumflex ... is being marked with a hard mark ô -> оъ
+	- And the soft ones are used to mark ´, like ó -> оь, except the é since it has its own equivalent character in cyrrilic é -> э
 
-local function transliterate(text) -- by now only from latin to cyrrilic...
+]]
+
+local insp = require "inspect"
+
+local function transliterate(script, text) -- by now only from latin to cyrrilic...
 	local translit = ''
+	local converted = utf8.codes(text)
 
-	for _, char in utf8.codes(text) do
+	if script == cyrrilic then
+		for cyr, lat in pairs(script.special) do
+			text = string.gsub(text, cyr, lat)
+		end
+	end -- the special cases must be handled before the main process! why?? idk
+
+	for pos, char in utf8.codes(text) do
 		local c = utf8.char(char)
 
-		if letters[c] then
-			translit = translit .. letters[c]   -- memoized...
-		elseif not letters[c] then
+		if script[c] then
+			translit = translit .. script[c]
+		elseif not script[c] then
 			translit = translit .. c
 		end
 	end
 
 	return translit
 end
+
+local latin_sample = [[Quando errar, quando assar faça sintaxe.]]
+local cyrrilic_sample = [[оь е ай, э тамбэм уьлтимо дос кс онъ]]
+
+print(transliterate(cyrrilic, cyrrilic_sample))
+-- print(transliterate(latin, latin_sample))
+
 
 return transliterate
