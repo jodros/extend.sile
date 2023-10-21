b76df054299ed2925634730a97faa18c8f0d444e
jodros <15092760+jodros@users.noreply.github.com>
2023-10-21 15:34:24 -0300

    test: tests folder created

--- tests/full-page.lua
+++ tests/full-page.lua
@@ -1,17 +1,11 @@
-return function()
-  --  local plain = require "classes.extend.plain"
-  local plain = require "classes.plain"
+return function ()
+  local plain = require "classes.extend.plain"
   local class = plain()
   SILE.documentState.documentClass = class
-  local insp = require "inspect"
 
-  for key, value in pairs(class.packages) do
-    print(key)
-  end
+  SILE.call("typeset-into", { frame = "page" }, function (_, _)
+    SILE.call("img", { src = "resources/parana-river.jpg" })
+  end)
 
-  --  SILE.call("typeset-into", { frame = "page" }, function (_, _)
-  --    SILE.call("img", { src = "resources/parana-river.jpg" })
-  --  end)
-  --
-  --  SILE.call("supereject")
+  SILE.call("supereject")
 end
