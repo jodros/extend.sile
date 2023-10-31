eb968dfd9e2623447dcc04412b428369495577df
jodros <15092760+jodros@users.noreply.github.com>
2023-10-30 22:29:09 -0300

    test: started to play with multiple tomls

--- tests/full-page.lua
+++ tests/full-page.lua
@@ -1,11 +1,17 @@
-return function ()
-  local plain = require "classes.extend.plain"
+return function()
+  --  local plain = require "classes.extend.plain"
+  local plain = require "classes.plain"
   local class = plain()
   SILE.documentState.documentClass = class
+  local insp = require "inspect"
 
-  SILE.call("typeset-into", { frame = "page" }, function (_, _)
-    SILE.call("img", { src = "resources/parana-river.jpg" })
-  end)
+  for key, value in pairs(class.packages) do
+    print(key)
+  end
 
-  SILE.call("supereject")
+  --  SILE.call("typeset-into", { frame = "page" }, function (_, _)
+  --    SILE.call("img", { src = "resources/parana-river.jpg" })
+  --  end)
+  --
+  --  SILE.call("supereject")
 end
