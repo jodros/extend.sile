3c0662e2c3e24d7c5c533d47b20a57c498481269
jodros <15092760+jodros@users.noreply.github.com>
2023-11-17 14:51:51 -0300

    extra-textual to be completely rewritten

--- packages/extra-textual/init.lua
+++ packages/extra-textual/init.lua
@@ -1,11 +1,8 @@
 local base = require "packages.base"
 local inspect = require "inspect"
 
-
 local package = pl.class(base)
 package._name = "extra-textual"
-
-
 
 function package:_init()
   base._init(self)
@@ -13,55 +10,7 @@ end
 
 function package:registerCommands()
   self:registerCommand("front", function(options, content) -- fronstispiece
-    options.id              = options.id or "front"
-    options.title           = options.title or SILE.scratch.book.title or " "
-    options.author          = options.author or SILE.scratch.book.author or " "
-    options.imprint         = options.imprint or SILE.scratch.book.imprint or " "
-    options.backgroundColor = options.backgroundColor or SILE.scratch.styles.colors.front or "#00000"
-    -- SILE.call("background", {  allpages = false, color = options.backgroundColor })
-    SILE.call("switch-master-one-page", { id = options.id })
-
-
-    options.linedistance = options.linedistance or SILE.scratch.styles.linedistance or "15pt"
-
-    local fontfamily     = function(field)
-      return options.fontfamily or SILE.scratch.styles.fonts[field][1] or "Cormorant"
-    end
-
-    local fontsize       = function(field)
-      return options.fontsize or SILE.scratch.styles.fonts[field][2] or "16pt"
-    end
-
-    local fontweight     = function(field)
-      return options.fontsize or SILE.scratch.styles.fonts[field][3] or "400"
-    end
-
-    local justification  = function(field)
-      return options.justification or SILE.scratch.styles.alingments[field] or "center"
-    end
-
-    local frames         = { "author", "title", "subtitle", "imprint", "isbn" } -- possible frames for cover; to be improved
-
-    SILE.call("nofolios")
-
-    if content then
-      SILE.process(content)
-    end
-
-    for _, frame in ipairs(frames) do
-      if SILE.scratch.styles.frames.front.frameset[frame] then
-        SILE.call("align", { item = justification(frame) }, function()
-          SILE.call("front-item",
-            { id = frame, family = fontfamily(frame), size = fontsize(frame), weight = fontweight(frame) },
-            function()
-              SILE.typesetter:typeset(options[frame])
-            end)
-        end)
-      end
-    end
-
-    SILE.call("supereject")
-  end, "")
+  end)
 end
 
 package.documentation = [[
@@ -69,8 +18,7 @@ package.documentation = [[
 
 \chapter{Extra-textual}
 
-What the \autodoc:package{extra-textual} package does is \dash as its name suggests \dash create the parts of a book that are not part of the textual body, like the cover, printing and edition notice, ...
-
+What the \autodoc:package{extra-textual} package does is \dash as its name suggests \dash create the parts of a book that are not part of the textual body, like the cover, and edition notice, ...
 
 \end{document}
 ]]
