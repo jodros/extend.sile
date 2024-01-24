2c5a0eaad674a59305f5a3c28ac7411b624df706
jodros <15092760+jodros@users.noreply.github.com>
2024-01-24 05:13:33 -0300

    feat(packages): ignore if there's no folio frame

--- packages/extend/folio/init.lua
+++ packages/extend/folio/init.lua
@@ -7,6 +7,8 @@ function package.incrementFolio(_)
   SILE.scratch.counters.folio.value = SILE.scratch.counters.folio.value + 1
 end
 
+local isFolioFrame, folioFrame = nil, {} 
+
 function package:outputFolio(frame)
   if not frame then frame = "folio" end
   local folio = self.class.packages.counters:formatCounter(SILE.scratch.counters.folio)
@@ -16,7 +18,7 @@ function package:outputFolio(frame)
       SILE.scratch.counters.folio.off = false
     end
   else
-    local folioFrame = SILE.getFrame(frame)
+    isFolioFrame, folioFrame = pcall(SILE.getFrame(frame))
     if (folioFrame) then
       SILE.typesetNaturally(folioFrame, function()
         SILE.settings:pushState()
@@ -46,7 +48,7 @@ function package:_init(options)
   self.class:loadPackage("counters")
   SILE.scratch.counters.folio = { value = 1, display = "arabic" }
   self.class:registerHook("newpage", function() self:incrementFolio() end)
-  self.class:registerHook("endpage", function() self:outputFolio(options and options.frame) end)
+  self.class:registerHook("endpage", function () if isFolioFrame then self:outputFolio(options and options.frame) end end)
   self:export("outputFolio", self.outputFolio)
 end
 
