f63ae84829e938d6528eb9f94df796265c1f097b
jodros <15092760+jodros@users.noreply.github.com>
2024-01-24 04:56:06 -0300

    feat(packages): 'front' command

--- packages/extra-textual/init.lua
+++ packages/extra-textual/init.lua
@@ -9,7 +9,42 @@ function package:_init()
 end
 
 function package:registerCommands()
+  local front = SILE.scratch.config.frames.front
+
+  if front then
+    for name in pairs(front) do
+      self:registerCommand(name, function (options, content)
+        SILE.call("typeset-into", { frame = name }, function ()
+            SILE.process(content)
+        end)
+      end)
+    end
+  end
+
   self:registerCommand("front", function(options, content) -- fronstispiece
+    if front then
+      SILE.call("pagetemplate", { ["first-content-frame"] = "title" }, function ()
+        for name, frame in pairs(front) do
+          SILE.call("frame", {
+            id = name,
+            x = frame.x,
+            y = frame.y,
+            width = frame.width,
+            height = frame.height,
+            top = frame.top,
+            bottom = frame.bottom,
+            left = frame.left,
+            right = frame.right
+          })
+        end
+      end)
+
+      SILE.process(content)
+
+      SILE.call("pagebreak")
+    else
+      SU.error("No 'front' frameset declared!")
+    end
   end)
 end
 
--- tests/extra-textual/cover.lua
+++
@@ -1,7 +0,0 @@
-return function()
-    local plain = require "classes.plain"
-    local class = plain()
-    SILE.documentState.documentClass = class
-
-
-end
