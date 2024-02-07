e48d6ec89d26790f17975c6aa2672905891fa67b
jodros <15092760+jodros@users.noreply.github.com>
2024-02-07 00:53:05 -0300

    little changes in extra-textual, nothing serious...

--- packages/extra-textual/init.lua
+++ packages/extra-textual/init.lua
@@ -15,40 +15,45 @@ function package:registerCommands()
     for name in pairs(front) do
       self:registerCommand(name, function (options, content)
         SILE.call("typeset-into", { frame = name }, function ()
-          if (SILE.scratch.config.book[name] and SILE.scratch.styles.fonts[name]) then
             SILE.call("align", { item = name }, function ()
               SILE.call("font:"..name, {}, function ()
-                SILE.typesetter:typeset(SILE.scratch.config.book[name])
+                if content[1] then
+                  SILE.process(content) 
+                elseif not content[1] and SILE.scratch.config.book[name] then
+                  SILE.typesetter:typeset(SILE.scratch.config.book[name])
+                else
+                  SU.error("STUPID ERROR") -- I know this line should be different but I'm lazy
+                end
               end)
             end)
-          else
-            SILE.process(content)
-          end
+          end)
         end)
-      end)
-    end
+     end
   end
 
   self:registerCommand("front", function(options, content) -- fronstispiece
+    if options.backcolor then SILE.call("background", {allpages=false, color=options.backcolor}) end
+    
     if front then
-      SILE.call("pagetemplate", { ["first-content-frame"] = "title" }, function ()
+      SILE.call("pagetemplate", { ["first-content-frame"] = "author" }, function ()
         for name, frame in pairs(front) do
           SILE.call("frame", {
             id = name,
-            x = frame.x or SILE.scratch.styles.alingments[name],
+            x = frame.x or SILE.scratch.styles.alingments.front.x,
             y = frame.y,
             width = frame.width,
             height = frame.height,
             top = frame.top,
             bottom = frame.bottom,
-            left = frame.left,
-            right = frame.right
+            left = frame.left or SILE.scratch.styles.alingments.front.left,
+            right = frame.right or SILE.scratch.styles.alingments.front.right
           })
         end
       end)
 
       SILE.process(content)
 
+      SILE.call("pagebreak")
       SILE.call("pagebreak")
     else
       SU.error("No 'front' frameset declared!")
