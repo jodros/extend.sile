ade6324032038de2a1f5a01bb066223671c10aa1
jodros <15092760+jodros@users.noreply.github.com>
2024-02-08 00:41:01 -0300

    chore(tests): Remove tests

--- tests/bilingual/main.lua
+++
@@ -1,8 +0,0 @@
-return function()
-    local plain = require "classes.hooking"
-    local class = plain()
-    SILE.documentState.documentClass = class
-    
-    -- SILE.call("font:headers")
-    SILE.call("ellipsis")
-end
