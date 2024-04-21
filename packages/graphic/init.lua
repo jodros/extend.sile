2233143c5588644c34965726dc90ae34ef05691a
jodros <15092760+jodros@users.noreply.github.com>
2024-04-21 03:06:21 -0300

    feat(packages): pic command 'done'

--- packages/graphic/init.lua
+++ packages/graphic/init.lua
@@ -10,21 +10,31 @@ end
 function package:registerCommands()
     self:registerCommand("pic", function (options, content)
         local align = options.align or "center"
-        local width = options.width
-        function wcheck() if not width then return "27%ph" end end
-        local height = options.height or wcheck()
+        local width = options.width or SILE.scratch.config.pic.width
+        local height = options.height or SILE.scratch.config.pic.height
+        local skip = options.skip or SILE.scratch.config.pic.skip
 
-        SILE.settings:temporarily(function ()
+        if SU.boolean(options.full) then
+          SILE.call("typeset-into", { frame = "page" }, function()
+              SILE.call("noindent")
+              SILE.call("img", { src = options.src, height = "100%ph" })
+          end) 
+        else
+         SILE.settings:temporarily(function ()
             SILE.settings:set("document.parskip", "1pt")
 
-            SILE.call("align", { item = align }, function ()
-                SILE.call("img", { src = options.src, width = width, height = height })
-                SILE.call("par")
-                SILE.call("font:caption", {}, content)
-                SILE.call("par")
-            end)  
-        end)
+            SILE.call("double-skip", { height = skip }, function()
+              SILE.call("align", { item = align }, function ()
+                  SILE.call("img", { src = options.src, width = width, height = height })
+                  SILE.call("par")
+                  SILE.call("font:caption", {}, content)
+                  SILE.call("par")
+              end)  
+            end)
+         end)
+       end
    end)
 end
 
 return package
+
